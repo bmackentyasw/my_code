28b0da65938030500d9d4e723cf0ab4cfac17aff
bmackentyasw <bmackenty@aswarsaw.org>
2015-11-03 09:21:44 +0100

    Create list_classes.py

---
+++ list_classes.py
@@ -0,0 +1,13 @@
+classes = ['math','science','advanced math', 'ss', 'la', 'spanish']
+my_schedule = []
+
+for i,p in enumerate(classes):
+       print str(i) + ". " + p  
+       
+my_classes = input("What class do you have A block? ")
+
+if my_classes in range(len(classes)):
+    print("this is a valid choice")
+    my_schedule.append(classes.pop(my_classes))
+else: 
+    print("this is not a valid choice")    
